9850552bd3edff401220a6fddd69e789868494aa
Antoine <antoine.thiel@hotmail.fr>
2023-08-15 14:53:21 +0200

    init screens, packages, models

---
+++ android/app/src/main/kotlin/com/example/useradgent/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.useradgent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/burger.dart
@@ -0,0 +1,25 @@
+class Burger {
+  String ref;
+  String title;
+  String description;
+  String imageUrl;
+  int price;
+
+  Burger({
+    required this.ref,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.price,
+  });
+
+  factory Burger.fromJson(Map<String, dynamic> json) {
+    return Burger(
+      ref: json['ref'],
+      title: json['title'],
+      description: json['description'],
+      imageUrl: json['thumbnail'],
+      price: json['price'],
+    );
+  }
+}
